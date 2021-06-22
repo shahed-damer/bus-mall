528ee70d402feb3916d835129aad7060eb7504db
shahed qerbaa <shaheddamer@gmail.com>
2021-06-23 00:34:21 +0300

    Lab13-LocalStorage

--- js/bus.js
+++ js/bus.js
@@ -2,165 +2,147 @@
 
 
 
-let imgSection =document.getElementById('imgSection');
+let imgSection = document.getElementById('imgSection');
 let leftImage = document.getElementById('leftImage');
 let centerImage = document.getElementById('centerImage');
 let rightImage = document.getElementById('rightImage');
-let results = document.getElementById('results');
+let list1 = document.getElementById('list1');
 let viewResults = document.getElementById('viewResults');
 
-function ProdectImages(product , srcc){
-    this.product = product ;
+function ProdectImages(product, srcc,views,clicks) {
+    this.product = product;
     this.srcc = `./assets/${srcc}`;
-    this.views = 0;
-    this.click = 0;
+    this.views = views;
+    this.click = clicks;
     ProdectImages.all.push(this);
 }
 
 ProdectImages.all = [];
 
-let productImage = ['bag.jpg', 'banana.jpg','bathroom.jpg','boots.jpg',
-'breakfast.jpg','bubblegum.jpg','chair.jpg','cthulhu.jpg','dog-duck.jpg',
-'dragon.jpg','pen.jpg','pet-sweep.jpg','scissors.jpg',
-'shark.jpg','sweep.png','tauntaun.jpg','unicorn.jpg','usb.gif' , 'water-can.jpg' ,'wine-glass.jpg'];
+let productImage = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg',
+    'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg',
+    'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg',
+    'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
 
 
 
 
-function randomNumber( min, max ) {
-    min = Math.ceil( min );
-    max = Math.floor( max );
-    return Math.floor( Math.random() * ( max - min + 1 ) + min ); 
-    }
-    
+function randomNumber(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 
 let counter = 0;
-let round =0;
+let round = 0;
 
 
-ProdectImages.all =[];
+ProdectImages.all = [];
 
-for (let i = 0; i <  productImage.length; i++) {
-   new ProdectImages(productImage[i].split('.')[0],productImage[i]);
-}
 
 let arry = [];
 
 let rightIndex;
 let centerIndex;
-let leftIndex ;
-function render(){
-
-
- 
-    leftIndex = randomNumber(0,productImage.length -1);
-
-    centerIndex = randomNumber(0,productImage.length -1);
-
-  rightIndex = randomNumber(0, productImage.length -1);
- while( leftIndex === centerIndex || rightIndex=== leftIndex || rightIndex === centerIndex || arry.includes(leftIndex ) || arry.includes(centerIndex) || arry.includes(rightIndex) ){
-    leftIndex = randomNumber(0,productImage.length -1);
-
-    centerIndex = randomNumber(0,productImage.length -1);
-
-  rightIndex = randomNumber(0, productImage.length -1);
- }
-
-rightImage.src =  ProdectImages.all[rightIndex].srcc;
-leftImage.src =  ProdectImages.all[leftIndex].srcc;
-centerImage.src =  ProdectImages.all[centerIndex].srcc;
-arry=[];
-arry.push(leftIndex);
-arry.push(centerIndex);
-arry.push(rightIndex);
-
-console.log(arry)
-
-ProdectImages.all[leftIndex].views++;
-ProdectImages.all[centerIndex].views++;
-ProdectImages.all[rightIndex].views++;
+let leftIndex;
+function render() {
 
 
 
+    leftIndex = randomNumber(0, productImage.length - 1);
+
+    centerIndex = randomNumber(0, productImage.length - 1);
+
+    rightIndex = randomNumber(0, productImage.length - 1);
+    while (leftIndex === centerIndex || rightIndex === leftIndex || rightIndex === centerIndex || arry.includes(leftIndex) || arry.includes(centerIndex) || arry.includes(rightIndex)) {
+        leftIndex = randomNumber(0, productImage.length - 1);
+
+        centerIndex = randomNumber(0, productImage.length - 1);
+
+        rightIndex = randomNumber(0, productImage.length - 1);
+    }
+
+    rightImage.src = ProdectImages.all[rightIndex].srcc;
+    leftImage.src = ProdectImages.all[leftIndex].srcc;
+    centerImage.src = ProdectImages.all[centerIndex].srcc;
+    arry = [];
+    arry.push(leftIndex);
+    arry.push(centerIndex);
+    arry.push(rightIndex);
+
+  
+
+    ProdectImages.all[leftIndex].views++;
+    ProdectImages.all[centerIndex].views++;
+    ProdectImages.all[rightIndex].views++;
 
 
-// console.log(ProdectImages.all);
+
+
+
+    // console.log(ProdectImages.all);
 }
 
 function eventHandler(e) {
 
-if((e.target.id === 'leftImage' || e.target.id === 'centerImage' || e.target.id === 'rightImage') && counter < 25){
-    
-    if (e.target.id ==='leftImage' ){ 
-        
-ProdectImages.all[leftIndex].click++;
+    if ((e.target.id === 'leftImage' || e.target.id === 'centerImage' || e.target.id === 'rightImage') && counter < 25) {
 
+        if (e.target.id === 'leftImage') {
 
+            ProdectImages.all[leftIndex].click++;
+        }
 
+        if (e.target.id === 'centerImage') {
+
+            ProdectImages.all[centerIndex].click++;
+        }
+
+        if (e.target.id === 'rightImage') {
+
+            ProdectImages.all[rightIndex].click++;
+
+        }
+        render();
+        counter++;
+
+    } else if (counter == 25) {
+        viewResults.disabled = false
+        imgSection.removeEventListener('clcik', eventHandler);
+        bottomChart();
+        storage();
 
     }
-
-    if (e.target.id ==='centerImage'){
-
-        ProdectImages.all[centerIndex].click++;
-    }
-
-    if (e.target.id ==='rightImage'){
-
-ProdectImages.all[rightIndex].click++;
-
-    }
-  render();
-  counter++;
-//   storage();
-
-} else  if(counter==25) { 
-    viewResults.disabled=false
-    imgSection.removeEventListener('clcik',eventHandler);
-    bottomChart();
-
-}
 
 }
 
 imgSection.addEventListener('click', eventHandler);
 
-render();
 
-
-function view(){
-
-
+function view() {
     let cont = document.getElementById('results')
-
     let list = document.createElement('ul');
     cont.appendChild(list);
     for (let i = 0; i < productImage.length; i++) {
         let list1 = document.createElement('li');
         list.appendChild(list1);
-
-    list1.textContent=(`${productImage[i].split('.')[0]} had ${ ProdectImages.all[i].click}  votes, and was seen ${ ProdectImages.all[i].views} times.`)
-
-        
+        list1.textContent = (`${productImage[i].split('.')[0]} had ${ProdectImages.all[i].click}  votes, and was seen ${ProdectImages.all[i].views} times.`)
     }
 }
 
+function bottomChart() {
+    let prodect = [];
+    let vieww = [];
+    let clickss = [];
 
+    for (let i = 0; i < ProdectImages.all.length; i++) {
+        prodect.push(ProdectImages.all[i].product);
+        vieww.push(ProdectImages.all[i].views);
+        clickss.push(ProdectImages.all[i].click);
+    }
 
-
-function bottomChart(){
-    let prodect=[];
-    let vieww=[];
-    let clickss=[];
-    
-    for (let i = 0; i< ProdectImages.all.length; i++) {
-      prodect.push(ProdectImages.all[i].product);
-      vieww.push(ProdectImages.all[i].views);
-      clickss.push(ProdectImages.all[i].click);
-        }
-    
     let ctx = document.getElementById('myChart').getContext('2d');
-    let myChart = new Chart(ctx, {
+    new Chart(ctx, {
         type: 'bar',
         data: {
             labels: prodect,
@@ -184,7 +166,7 @@ function bottomChart(){
                     'rgba(255, 159, 64, 1)'
                 ],
                 borderWidth: 1
-            },{
+            }, {
                 label: '# of click',
                 data: clickss,
                 backgroundColor: [
@@ -205,8 +187,8 @@ function bottomChart(){
                 ],
                 borderWidth: 1
             }]
-            
-    },
+
+        },
         options: {
             scales: {
                 y: {
@@ -215,21 +197,25 @@ function bottomChart(){
             }
         }
     });
+}
+
+
+
+
+function storage() {
+    localStorage.setItem('imgestorage', JSON.stringify(ProdectImages.all))
+}
+
+function getData() {
+let dta =  JSON.parse(localStorage.getItem('imgestorage'))
+if (dta){
+   
+    ProdectImages.all = dta
+    render();
+} else {
+    for (let i = 0; i < productImage.length; i++) {
+        new ProdectImages(productImage[i].split('.')[0], productImage[i],0,0);
     }
-
-
-
-
-
-      function storage(){
-          let datm = JSON.stringify( ProdectImages.all);
-          localStorage.setItem('imgeestorage', datm)
-
-      }
-
-      function store(){
-        ProdectImages.all= JSON.parse(localStorage.getItem('imgeestorage'))
-        
-        
-      }
-      store();
+    render();
+}}
+getData();
